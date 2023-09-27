bf32a0afb12599f1f9a9877b55c8ad905658b428
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2023-09-27 13:52:46 -0400

    Update average_active.jl

--- src/theory_check/average_active.jl
+++ src/theory_check/average_active.jl
@@ -69,11 +69,10 @@ for i in 1:19
 end
 println(res1)
 println(res2)
-#=
+
 #plot figure
 plot(10^6:5*10^5:10^7,res1, linewidth=1, label="observed average", line = (:blue,1,:scatter), xlabel = "input size n",
        ylabel ="active elements",legend=:topleft)
 plot!(10^6:5*10^5:10^7,res2,label="estimate", line=(:orange, 1, 1))
 savefig("average_active.png")
 println("Note: generate a new figure named average_active.png")
-=#
