c166c8f04430d9c27f26abcc07377b318a7c72c1
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2023-09-29 10:38:31 -0400

    Update l1ball_runtime_benchmark.jl

--- src/l1ball_runtime_benchmark.jl
+++ src/l1ball_runtime_benchmark.jl
@@ -19,9 +19,11 @@ function l1ball_test_condat(n::Int)
     println("results for condat:")
     Random.seed!(12345); res = @benchmark l1ball_condat_s($(rand(Normal(0, 1), n)), 1)
     println("serial : ", median(res))
+    res_list = []
     for i in 1:10
         Random.seed!(12345); res = @benchmark l1ball_condat_p($(rand(Normal(0, 1), n)), 1, $(i*8), 0.001)
         println(i*8, " : ", median(res))
+        push!(res_list, median(res))
     end
 end
 
@@ -29,9 +31,11 @@ function l1ball_test_michelot(n::Int)
     println("results for michelot:")
     Random.seed!(12345); res = @benchmark l1ball_michelot_s($(rand(Normal(0, 1), n)), 1)
     println("serial : ", median(res))
+    res_list = []
     for i in 1:10
         Random.seed!(12345); res = @benchmark l1ball_michelot_p($(rand(Normal(0, 1), n)), 1, $(i*8), 0.0)
         println(i*8, " : ", median(res))
+        push!(res_list, median(res))
     end
 end
 
@@ -39,14 +43,20 @@ function l1ball_test_sortscan(n::Int)
     println("results for sortscan:")
     Random.seed!(12345); res = @benchmark l1ball_sortscan_s($(rand(Normal(0, 1), n)), 1)
     println("serial : ", median(res))
+    res_list = []
     for i in 1:10
         Random.seed!(12345); res = @benchmark l1ball_sortscan_p($(rand(Normal(0, 1), n)), 1, $(i*8))
         println(i*8, " sortscan : ", median(res))
         Random.seed!(12345); res = @benchmark l1ball_sortPscan_p($(rand(Normal(0, 1), n)), 1, $(i*8))
         println(i*8, " sortPscan: ", median(res))
+        push!(res_list, median(res))
     end
 end
 
-l1ball_test_sortscan(10^8)
-l1ball_test_michelot(10^8)
-l1ball_test_condat(10^8)
+function get_result()
+    res_ss = l1ball_test_sortscan(10^8)
+    res_m = l1ball_test_michelot(10^8)
+    res_c = l1ball_test_condat(10^8)
+    return res_ss, res_m, res_c
+end
+
