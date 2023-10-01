b370090befb36360e3722508a3da94fbb75daafc
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2023-10-01 19:23:26 -0400

    Create data_combine.jl

---
+++ data/data_combine.jl
@@ -0,0 +1,23 @@
+#data_combine
+
+begin
+    data = []
+    for i in 1:8
+        sub_data = readdlm("kdd10/kdd10_"*string(i)*".txt", '\n')
+        append!!(data, sub_data)
+    end
+    open("kdda.txt", "w") do io
+        writedlm(io, data)
+    end
+end
+
+begin
+    data = []
+    for i in 1:200
+        sub_data = readdlm("kdd12/kdd12_"*string(i)*".txt", '\n')
+        append!!(data, sub_data)
+    end
+    open("kdd12.txt", "w") do io
+        writedlm(io, data)
+    end
+end
