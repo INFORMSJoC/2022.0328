530c5a934212092508e68e4c7694fe00be84506d
foreverdyz <44935367+foreverdyz@users.noreply.github.com>
2023-10-01 14:27:54 -0400

    Update plot_graphs.py

--- src/plot_graphs.py
+++ src/plot_graphs.py
@@ -2,12 +2,12 @@
 
 import matplotlib.pyplot as plt
 
-def plot_absolute(runtime_matrix, fast_runtime, fig_name)
+def plot_absolute(runtime_matrix, fig_name)
   cores = [1, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80]
-  y1 = runtime_matrix[1, :]./fast_runtime
-  y2 = runtime_matrix[2, :]./fast_runtime
-  y3 = runtime_matrix[3, :]./fast_runtime
-  y4 = runtime_matrix[4, :]./fast_runtime
+  y1 = runtime_matrix[1, :]
+  y2 = runtime_matrix[2, :]
+  y3 = runtime_matrix[3, :]
+  y4 = runtime_matrix[4, :]
   plt.plot(cores[1:11], y4, 's-', label = "(P)Condat", color = "red")
   plt.plot(cores[1:11], y3, 'o-', label = "(P)Michelot", color = "blue")
   plt.plot(cores[1:11], [1]*10, '--', label = "(S)Condat", color = "grey")
@@ -23,9 +23,9 @@ def plot_absolute(runtime_matrix, fast_runtime, fig_name)
 
 def plot_relative_simplex(runtime_matrix, serial_runtime, fig_name)
   cores = [1, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80]
-  y1 = runtime_matrix[1, :]./serial_runtime[1]
-  y2 = runtime_matrix[2, :]./serial_runtime[2]
-  y3 = runtime_matrix[3, :]./serial_runtime[3]
+  y1 = runtime_matrix[1, :]
+  y2 = runtime_matrix[2, :]
+  y3 = runtime_matrix[3, :]
   plt.plot(cores[1:11], y3, 's-', label = r'$N(0,0.001)$', color = "red")
   plt.plot(cores[1:11], y2, '^-', label = r'$U[0,1]$', color = "green")
   plt.plot(cores[1:11], y1, 'o-', label = r'$N(0,1)$', color = "blue")
@@ -39,11 +39,10 @@ def plot_relative_simplex(runtime_matrix, serial_runtime, fig_name)
   
 def plot_relative_others(runtime_matrix, serial_runtime, fig_name)
   cores = [1, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80]
-  cores = [1, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80]
-  y1 = runtime_matrix[1, :]./serial_runtime[1]
-  y2 = runtime_matrix[2, :]./serial_runtime[2]
-  y3 = runtime_matrix[3, :]./serial_runtime[3]
-  y4 = runtime_matrix[4, :]./serial_runtime[4]
+  y1 = runtime_matrix[1, :]
+  y2 = runtime_matrix[2, :]
+  y3 = runtime_matrix[3, :]
+  y4 = runtime_matrix[4, :]
   plt.plot(cores[1:11], y4, 's-', label = "(P)Condat", color = "red")
   plt.plot(cores[1:11], y3, 'o-', label = "(P)Michelot", color = "blue")
   plt.plot(cores[1:11], y2, '<-', label = "(P)Sort and (P)Scan", color = "orange")
